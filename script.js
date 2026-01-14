8395b1a38e287e9f83be6943b8627b371f51276b
Md Asaduzzaman Rana <iamtheasad@gmail.com>
2026-01-14 13:42:54 +0600

    :wrench: ref: js

--- script.js
+++ script.js
@@ -513,7 +513,7 @@ console.log(h1.parentElement.children);
 });
  */
 
-document.addEventListener('DOMContentLoaded', function (e) {
+/* document.addEventListener('DOMContentLoaded', function (e) {
   console.log('HTML parsed and DOM tree built!', e);
 });
 
@@ -521,8 +521,8 @@ window.addEventListener('load', function (e) {
   console.log('After page fully loaded', e);
 });
 
-// window.addEventListener('beforeunload', function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   e.returnValue = true;
-// });
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = true;
+}); */
